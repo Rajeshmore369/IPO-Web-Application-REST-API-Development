7833201a16d30e5aa779e1bd4bec1a8cdc86d77a
Rajesh Bharat More <75035291+Rajeshmore369@users.noreply.github.com>
2024-09-10 12:57:10 +0530

    commit url changes

--- bluestockweb/upcomingipo/models.py
+++ bluestockweb/upcomingipo/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class UpcomingIPO(models.Model):
+    company_name=models.CharField(max_length=100,unique=True)
+    Price_band=models.IntegerField()
+    open_date=models.DateTimeField()
+    close_date=models.DateTimeField()
+    issue_size=models.IntegerField()
+    issue_type=models.CharField(max_length=100)
+    listing_date=models.DateTimeField()
+    status=models.CharField(max_length=100)
+action=models.IntegerField()
+    
+    
+    
