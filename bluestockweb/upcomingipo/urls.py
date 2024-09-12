0d6bf8ff0b8288216b231ed816f6aa1fea49e66e
Rajesh Bharat More <75035291+Rajeshmore369@users.noreply.github.com>
2024-09-12 19:33:02 +0530

    header commit

--- bluestockweb/upcomingipo/urls.py
+++ bluestockweb/upcomingipo/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-
+from django.conf import settings
 from .views import index
-
+from django.conf.urls.static import static
 urlpatterns = [
     path('',index),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
